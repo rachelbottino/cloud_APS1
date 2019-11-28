7186e32255cdf26e7e9c5116597c2eb822b66682
rachelbottino <rachelpbm@gmail.com>
2019-11-27 22:25:06 -0300

    route funcionando

--- route.py
+++ route.py
@@ -1,17 +1,19 @@
 import requests
-from flask import Flask 
+from flask import Flask
+from flask import jsonify
 
-url = '18.204.216.201:5000/'
+ip = ''
+
+url = 'http://'+ip+':5000/'
 app = Flask(__name__)
 
 @app.route('/', defaults={'u_path': ''})
 @app.route('/<path:u_path>')
 def catch_all(u_path):
-    print(repr(u_path))
-    r = requests.get(url)
-	print(r.json())
-    print('You want path: %s' % u_path)
-    return(r.json())
+	print(url+u_path)
+	r = requests.get(url+u_path)
+	resp = jsonify(r.json())
+	return(resp)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
